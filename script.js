f4a30928d674bfc1be3ec1821d494c27f3a9595f
Yousuf-22 <yousuf.sayyed.cse@ghrcemp.raisoni.net>
2024-08-04 21:16:25 +0530

    NewProject

---
+++ script.js
@@ -0,0 +1,21 @@
+// const BASE_API = "https://api.frankfurter.app/currencies"
+const BASE_URL = "https://api.frankfurter.app/latest?amount=1&from=USD&to=INR"
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+
+
+
+for(let select of dropdowns) {
+    for (currcode in countryList) {
+        let newOption = document.createElement("option");
+        newOption.innerText = currcode;
+        newOption.value = currcode;
+        if(select.name == "from" && currcode == "USD") {
+            newOption.selected = "selected";
+        }
+        else if(select.name == "to" && currcode == "INR") {
+            newOption.selected = "selected";
+        }
+        select.append(newOption);
+    }
+}
